4955453fdd87958b8b2171f0c2b3697306bc8bb0
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-19 11:54:51 -0400

    test

--- app/parser.js
+++ app/parser.js
@@ -1,14 +1,19 @@
 class Parser {
-    constructor(){
-        this.commands = {}; 
-        this.data = ""; 
+    constructor() {
+        this.commands = {};
+        this.data = "";
     }
-    setData(data){
+    setData(data) {
         this.data = data;
     }
 
-    parseInput(){
-        let arrayMessage = this.data.split("\r\n");
+    parseInput() {
+        let arrayMessage = this.data.split("\r\n").filter(ele => ele.length > 0);
+        for (let i = 0; i < arrayMessage.length; i++) {
+            let valueArray = arrayMessage[i].split(" ");
+            console.log(valueArray);
+
+        }
         console.log(arrayMessage);
     }
 }

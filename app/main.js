6a9299bfec4859e242581497651081afff4d2cb3
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-19 14:33:02 -0400

    test

--- app/main.js
+++ app/main.js
@@ -6,6 +6,9 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+
+    const args = process.argv;
+    console.log(args)
     socket.on("close", () => {
         socket.end();
         server.close();

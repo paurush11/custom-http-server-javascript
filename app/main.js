501aff7d31176bd48fac05ac61c0776d0b705f65
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-19 11:24:21 -0400

    test

--- app/main.js
+++ app/main.js
@@ -9,6 +9,12 @@ const server = net.createServer((socket) => {
         socket.end();
         server.close();
     });
+
+    socket.on('data', (data) => {
+        console.log('Getting data:', data)
+        socket.write("HTTP/1.1 200 OK\r\n\r\n")
+        socket.end()
+    })
 });
 
 server.listen(4221, "localhost");
